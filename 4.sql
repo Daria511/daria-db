94e29a3a9c9a178cb839199041286a7329a38319
Daria511 <95719788+Daria511@users.noreply.github.com>
2021-12-12 17:11:02 +0200

    Update 4.sql

--- 4.sql
+++ 4.sql
@@ -6,10 +6,27 @@ insert all
     into Teachers (id_teacher, surname, name, fname) values (5, 'Сергінко', 'Ілля', 'Петрович')
 	into Teachers (id_teacher, surname, name, fname) values (6, 'Захарчук', 'Тетяна', 'Василівн')
 select * from dual;
+
+create or replace directory dir as 'D:\\';
+create table TeachersLoad(
+	id int,
+    name varchar(40),
+	surname varchar(40),
+	fname varchar(40)
+) organization external(
+    type ORACLE_LOADER
+    default directory dir
+    access parameters(
+        records delimited by newline
+        fields terminated by ','
+        missing field values are null
+    )
+    location ('load.txt')
+); insert into Teachers select * from TeachersLoad;
 	
 update Teachers set name = 'Дмитрієнко' where id_teacher = 4;
-update Teachers set surname = 'Сергієнко' where id_teacher >= 4 and id_teacher < 6;
+update Teachers set surname = 'Сергієнко' where id_teacher >= 5 and id_teacher <= 6;
 
 delete from Teachers where id_teacher = 4;
-delete from Teachers where id_teacher >= 4 and id_teacher < 6;
+delete from Teachers where id_teacher >= 5 and id_teacher <= 6;
 truncate table Teachers;
